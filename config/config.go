b3eaac6ae97629d117ba2c19111a4cec62abb356
Kevin Swiber <kswiber@gmail.com>
2016-08-25 22:05:21 -0700

    Moving to config package.

--- config/config.go
+++ config/config.go
@@ -12,13 +12,6 @@ type Config struct {
 	ProxyEndpoints []*ProxyEndpoint
 }
 
-type Proxy struct {
-	XMLName     string `xml:"APIProxy", hcl:",-"`
-	Name        string `xml:"name,attr,omitempty" hcl:",-"`
-	DisplayName string `xml:",omitempty" hcl:"display_name"`
-	Description string `xml:",omitempty" hcl:"description"`
-}
-
 type ProxyEndpoint struct {
 	XMLName string  `xml:"ProxyEndpoint" hcl:",-"`
 	Name    string  `xml:"name,attr" hcl:",-"`
@@ -78,25 +71,6 @@ func LoadConfigFromHCL(list *ast.ObjectList) (*Config, error) {
 	}
 
 	return &config, nil
-}
-
-func loadProxyHcl(list *ast.ObjectList) (*Proxy, error) {
-	//TODO: Check if more than one proxy.  Report error.
-	var item = list.Items[0]
-	n := item.Keys[0].Token.Value().(string)
-
-	if _, ok := item.Val.(*ast.ObjectType); !ok {
-		return nil, fmt.Errorf("proxy not an object")
-	}
-
-	var proxy Proxy
-	if err := hcl.DecodeObject(&proxy, item.Val); err != nil {
-		return nil, err
-	}
-
-	proxy.Name = n
-
-	return &proxy, nil
 }
 
 func loadProxyEndpointsHcl(list *ast.ObjectList) ([]*ProxyEndpoint, error) {
