006183bcde39bba67c554c4941160f5096856c52
Kevin Swiber <kswiber@gmail.com>
2016-09-02 16:25:12 -0700

    Adding support for StatisticsCollector policy.

---
+++ config/policy/statistics_collector.go
@@ -0,0 +1,146 @@
+package policy
+
+import (
+	"fmt"
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/hcl"
+	"github.com/hashicorp/hcl/hcl/ast"
+	"github.com/kevinswiber/apigee-hcl/config/hclerror"
+	"strings"
+)
+
+// StatisticsCollectorPolicy represents an <StatisticsCollector/> element.
+//
+// Documentation: http://docs.apigee.com/api-services/reference/statistics-collector-policy
+type StatisticsCollectorPolicy struct {
+	XMLName     string `xml:"StatisticsCollector" hcl:"-"`
+	Policy      `hcl:",squash"`
+	DisplayName string         `xml:",omitempty" hcl:"display_name"`
+	Statistics  []*scStatistic `xml:"Statistics>Statistic", hcl:"statistic"`
+}
+
+type scStatistic struct {
+	XMLName string `xml:"Statistic" hcl:"-"`
+	Name    string `xml:"name,attr" hcl:"-"`
+	Ref     string `xml:"ref,attr" hcl:"ref"`
+	Type    string `xml:"type,attr,omitempty" hcl:"type"`
+	Value   string `xml:",chardata" hcl:"value"`
+}
+
+// LoadStatisticsCollectorHCL converts an HCL ast.ObjectItem into an StatisticsCollectorPolicy object.
+func LoadStatisticsCollectorHCL(item *ast.ObjectItem) (interface{}, error) {
+	var errors *multierror.Error
+	var p StatisticsCollectorPolicy
+
+	if err := LoadCommonPolicyHCL(item, &p.Policy); err != nil {
+		return nil, err
+	}
+
+	var listVal *ast.ObjectList
+	if ot, ok := item.Val.(*ast.ObjectType); ok {
+		listVal = ot.List
+	} else {
+		pos := item.Val.Pos()
+		newError := hclerror.PosError{
+			Pos: pos,
+			Err: fmt.Errorf("statistics_collector policy not an object"),
+		}
+		return nil, &newError
+	}
+
+	if err := hcl.DecodeObject(&p, item.Val.(*ast.ObjectType)); err != nil {
+		return nil, err
+	}
+
+	if statsList := listVal.Filter("statistic"); len(statsList.Items) > 0 {
+		stats, err := loadStatisticsCollectorStatisticHCL(statsList.Items)
+		if err != nil {
+			errors = multierror.Append(errors, err)
+		} else {
+			p.Statistics = stats
+		}
+	}
+
+	if errors != nil {
+		return nil, errors
+	}
+
+	return p, nil
+}
+
+func loadStatisticsCollectorStatisticHCL(items []*ast.ObjectItem) ([]*scStatistic, error) {
+	var errors *multierror.Error
+	var stats []*scStatistic
+	for _, item := range items {
+		var stat scStatistic
+
+		if _, ok := item.Val.(*ast.ObjectType); !ok {
+			pos := item.Val.Pos()
+			newError := hclerror.PosError{
+				Pos: pos,
+				Err: fmt.Errorf("statistic not an object"),
+			}
+			return nil, &newError
+		}
+
+		if err := hcl.DecodeObject(&stat, item.Val.(*ast.ObjectType)); err != nil {
+			return nil, err
+		}
+
+		if len(item.Keys) == 0 || item.Keys[0].Token.Value().(string) == "" {
+			pos := item.Val.Pos()
+			newError := hclerror.PosError{
+				Pos: pos,
+				Err: fmt.Errorf("statistic requires a name"),
+			}
+			return nil, &newError
+		}
+
+		stat.Name = item.Keys[0].Token.Value().(string)
+
+		if stat.Ref == "" {
+			pos := item.Val.Pos()
+			newError := hclerror.PosError{
+				Pos: pos,
+				Err: fmt.Errorf("statistic requires a ref value"),
+			}
+			errors = multierror.Append(errors, &newError)
+		}
+
+		validTypes := []string{"string", "integer", "float", "long", "double"}
+		if stat.Type == "" {
+			pos := item.Val.Pos()
+			newError := hclerror.PosError{
+				Pos: pos,
+				Err: fmt.Errorf("statistic requires a type value"),
+			}
+			errors = multierror.Append(errors, &newError)
+		} else {
+			hasValidType := false
+			for _, t := range validTypes {
+				if stat.Type == t {
+					hasValidType = true
+					break
+				}
+			}
+
+			if !hasValidType {
+				pos := item.Val.Pos()
+				newError := hclerror.PosError{
+					Pos: pos,
+					Err: fmt.Errorf("statistic requires a valid type value [%s]",
+						strings.Join(validTypes, ", ")),
+				}
+				errors = multierror.Append(errors, &newError)
+			}
+		}
+
+		stats = append(stats, &stat)
+	}
+
+	if errors != nil {
+		return nil, errors
+	}
+
+	return stats, nil
+}
--- config/policy_list.go
+++ config/policy_list.go
@@ -7,13 +7,14 @@ import (
 
 // PolicyList is a map of HCL policy types to HCL loader functions.
 var PolicyList = map[string]func(*ast.ObjectItem) (interface{}, error){
-	"assign_message":    policy.LoadAssignMessageHCL,
-	"extract_variables": policy.LoadExtractVariablesHCL,
-	"javascript":        policy.LoadJavaScriptHCL,
-	"quota":             policy.LoadQuotaHCL,
-	"raise_fault":       policy.LoadRaiseFaultHCL,
-	"response_cache":    policy.LoadResponseCacheHCL,
-	"script":            policy.LoadScriptHCL,
-	"spike_arrest":      policy.LoadSpikeArrestHCL,
-	"verify_api_key":    policy.LoadVerifyAPIKeyHCL,
+	"assign_message":       policy.LoadAssignMessageHCL,
+	"extract_variables":    policy.LoadExtractVariablesHCL,
+	"javascript":           policy.LoadJavaScriptHCL,
+	"quota":                policy.LoadQuotaHCL,
+	"raise_fault":          policy.LoadRaiseFaultHCL,
+	"response_cache":       policy.LoadResponseCacheHCL,
+	"script":               policy.LoadScriptHCL,
+	"spike_arrest":         policy.LoadSpikeArrestHCL,
+	"statistics_collector": policy.LoadStatisticsCollectorHCL,
+	"verify_api_key":       policy.LoadVerifyAPIKeyHCL,
 }
